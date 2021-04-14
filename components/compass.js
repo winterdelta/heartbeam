183a9daef91887bf206c50e9cdad75562e6d2292
WΔ <w@winterdelta.com>
2021-04-14 16:51:43 +0100

    Plexify font.

--- components/compass.js
+++ components/compass.js
@@ -8,9 +8,16 @@ export default class compass extends Component {
   render() {
     return (
       <div>
+          <Head>
+              <link rel="preconnect" href="https://fonts.gstatic.com" />
+              <link
+                href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,100&display=swap"
+                rel="stylesheet"
+              />
+          </Head>
           <div className={styles.compass}>
                 <Link href="/">
-                <div className={styles.badge}></div>
+                <div className={styles.badge}>W∆</div>
                 </Link>
           </div>
       </div>
