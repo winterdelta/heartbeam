a780938d4c125a4ed62dbb8b77d5deb9f692561e
WΔ <w@winterdelta.com>
2021-04-14 16:10:27 +0100

    thinning of the font

--- components/hud.js
+++ components/hud.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import styles from "../styles/hud.module.css";
 import Legal from "./legal";
+import Head from 'next/head'
 
 class hud extends Component {
   render() {
     return (
       <div className={styles.hud}>
+          <Head>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,100&display=swap" rel="stylesheet"/>            
+          </Head>
         <div>
             <span className={styles.bold}>MULTI-OS</span> HESPERIDÉ Q3/4
         </div>
