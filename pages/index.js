2e625a01de28e8a56c565896286e2e0a4940da1b
WΔ <w@winterdelta.com>
2021-04-14 16:43:51 +0100

    reduce whitespace.

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,49 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
-import Encompasser from '../components/encompasser'
+import Head from "next/head";
+import Layout from "../components/layout";
+import styles from "../styles/Home.module.css";
+import Encompasser from "../components/encompasser";
 
 export default function Home() {
   return (
-      <Layout>
-          <Head>
-            <title>W∆ | Winterdelta | Heartbeam</title>
-            <meta name="title" content="W∆ | Winterdelta | Heartbeam"/>
-            <meta name="description" content="Researcher + Encompasser + Sonar"/>
+    <Layout>
+      <Head>
+        <title>W∆ | Winterdelta | Heartbeam</title>
+        <meta name="title" content="W∆ | Winterdelta | Heartbeam" />
+        <meta name="description" content="Researcher + Encompasser + Sonar" />
 
-            <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://www.winterdelta.com"/>
-            <meta property="og:title" content="W∆ | Winterdelta | Heartbeam"/>
-            <meta property="og:description" content="Researcher + Encompasser + Sonar"/>
-            <meta property="og:image" content="/Waves.png"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.winterdelta.com" />
+        <meta property="og:title" content="W∆ | Winterdelta | Heartbeam" />
+        <meta
+          property="og:description"
+          content="Researcher + Encompasser + Sonar"
+        />
+        <meta property="og:image" content="/Waves.png" />
+        <meta property="twitter:card" content="/Waves.png" />
+        <meta property="twitter:url" content="https://www.winterdelta.com" />
+        <meta property="twitter:title" content="W∆ | Winterdelta | Heartbeam" />
+        <meta
+          property="twitter:description"
+          content="Researcher + Encompasser + Sonar"
+        />
+        <meta property="twitter:image" content="/Waves.png" />
 
-            <meta property="twitter:card" content="/Waves.png"/>
-            <meta property="twitter:url" content="https://www.winterdelta.com"/>
-            <meta property="twitter:title" content="W∆ | Winterdelta | Heartbeam"/>
-            <meta property="twitter:description" content="Researcher + Encompasser + Sonar"/>
-            <meta property="twitter:image" content="/Waves.png"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,100&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <video
+        className={styles.video}
+        src="/CloudsBlue.mp4"
+        autoPlay
+        loop
+        playsInline
+        muted
+      />
 
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,100&display=swap" rel="stylesheet"/>
-            
-          </Head>
-            <video className={styles.video} src="/CloudsBlue.mp4" autoPlay loop playsInline muted/>
-
-          <Encompasser/>
-
-      </Layout>
-  )
+      <Encompasser />
+    </Layout>
+  );
 }
